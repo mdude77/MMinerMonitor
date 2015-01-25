ca2b9ba0132dc7d211cec3fe7b3aa56bc74613a3
Nate Woolls <nwoolls@gmail.com>
2015-01-24 23:38:42 -0500

    Fix / consistent namespaces for VS2012+ support

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.MAntMonitor.My.MySettings
+        Friend ReadOnly Property Settings() As Global.MMinerMonitor.My.MySettings
             Get
-                Return Global.MAntMonitor.My.MySettings.Default
+                Return Global.MMinerMonitor.My.MySettings.Default
             End Get
         End Property
     End Module
