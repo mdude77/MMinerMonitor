39bf16edccc868acdfe7da53b7527f7c4f8a9d8a
mdude77 <mdude77@verizon.net>
2015-02-07 20:56:03 -0500

    sync up build files

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.MAntMonitor.frmMain
+            Me.MainForm = Global.MMinerMonitor.frmMain
         End Sub
     End Class
 End Namespace
